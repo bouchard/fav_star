693029e8af44235d68632d752ce8ea5d652293f8
Mateusz Drożdżyński <matid@matid.net>
2010-07-22 16:23:19 +0200

    Removed the 'vote_fu inititalised properly message'

--- lib/vote_fu.rb
+++ lib/vote_fu.rb
@@ -5,12 +5,3 @@ require 'has_karma'
 ActiveRecord::Base.send(:include, Juixe::Acts::Voteable)
 ActiveRecord::Base.send(:include, PeteOnRails::Acts::Voter)
 ActiveRecord::Base.send(:include, PeteOnRails::VoteFu::Karma)
-
-success_message = '** vote_fu: initialized properly.'
-
-begin
-  RAILS_DEFAULT_LOGGER.info success_message
-rescue NameError
-  Logger.new(STDOUT).info success_message
-end
-

9dda88f4b38f0743dfb56635b56c5478f37b2a57
Brady Bouchard <brady@ldawn.com>
2010-09-01 16:13:52 +1000

    Well, whaddya know, Inflector gets it wrong.

--- lib/fav_star.rb
+++ lib/fav_star.rb
@@ -3,3 +3,8 @@ require 'acts_as_faver'
 
 ActiveRecord::Base.send(:include, FavStar::ActsAsFaveable)
 ActiveRecord::Base.send(:include, FavStar::ActsAsFaver)
+
+# Inflector gets this wrong, otherwise.
+ActiveSupport::Inflector.inflections do |i|
+  i.irregular 'fave', 'faves'
+end
